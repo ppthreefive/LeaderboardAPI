ecb266a829f52cfd2970c64c41e431345c4d11a0
Phillip Pham <ppthreefive@gmail.com>
2022-02-21 02:00:33 -0700

    Created a DTO object so that we can display according to what we want our data contract to be, not necessarily our models

---
+++ LeaderboardAPI/Dtos/LeaderboardDto.cs
@@ -0,0 +1,24 @@
+﻿using LeaderboardAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaderboardAPI.Dtos
+{
+    public class LeaderboardDto
+    {
+        public List<Entry> entries { get; set; }
+        public int page { get; set; }
+        public int totalPages { get; set; }
+        public int count { get; set; }
+
+        public LeaderboardDto(List<Entry> entries, int page, int count, int totalPages)
+        {
+            this.entries = entries;
+            this.page = page;
+            this.count = count;
+            this.totalPages = totalPages;
+        }
+    }
+}

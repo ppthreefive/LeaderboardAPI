30889129858639bd6a953a83601c91ca3dbf52e5
Phillip Pham <ppthreefive@gmail.com>
2022-02-21 02:03:37 -0700

    Roughly filled out the GetLeaderboard GET request method in our controller. Added dependency injection for default page size.

--- LeaderboardAPI/Controllers/LeaderboardController.cs
+++ LeaderboardAPI/Controllers/LeaderboardController.cs
@@ -1,7 +1,10 @@
-﻿using LeaderboardAPI.Models;
+﻿using LeaderboardAPI.Dtos;
+using LeaderboardAPI.Models;
 using LeaderboardAPI.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,16 +17,53 @@ namespace LeaderboardAPI.Controllers
     public class LeaderboardController : ControllerBase
     {
         private readonly ILeaderboardService leaderboardService;
+        private readonly int defaultPageSize;
 
-        public LeaderboardController(ILeaderboardService leaderboardService) 
+        public LeaderboardController(ILeaderboardService leaderboardService, string defaultPageSize) 
         {
             this.leaderboardService = leaderboardService;
+            this.defaultPageSize = int.Parse(defaultPageSize);
         }
 
         [HttpGet]
-        public ActionResult<List<Entry>> GetLeaderboard() 
+        public ActionResult<LeaderboardDto> GetLeaderboard([FromQuery] string page, [FromQuery] string count) 
         {
-            return leaderboardService.getAllEntries();
+            // TODO: Clean up the controller, can probably move some of this logic to the service
+            int total = leaderboardService.getTotalCount();
+
+            if (String.IsNullOrEmpty(page) && String.IsNullOrEmpty(count))
+            {
+                int remainder = total % this.defaultPageSize;
+                int totalPages = total / this.defaultPageSize;
+                if (remainder != 0)
+                {
+                    totalPages++;
+                }
+
+                return new LeaderboardDto(leaderboardService.getEntriesPaginated(0, this.defaultPageSize), 1, this.defaultPageSize, totalPages);
+            }
+            else
+            {
+                int pageSize = int.Parse(count);
+                int pageNum = int.Parse(page) - 1;
+                int start = pageSize * pageNum;
+                int remainder = total % pageSize;
+                int totalPages = total / pageSize;
+
+                if (remainder != 0)
+                {
+                    totalPages++;
+                }
+
+                if ((pageNum + 1) == totalPages)
+                {
+                    start = (pageNum - 1) * pageSize;
+
+                    return new LeaderboardDto(leaderboardService.getEntriesPaginated(start, remainder), pageNum, pageSize, totalPages);
+                }
+
+                return new LeaderboardDto(leaderboardService.getEntriesPaginated(start, pageSize), pageNum + 1, pageSize, totalPages);
+            }
         }
     }
 }

f1593ae4aec3918bbfe575d17bf3a7fdb8641017
Phillip Pham <ppthreefive@gmail.com>
2022-02-21 00:40:42 -0700

    Added interface and service with some implementation

---
+++ LeaderboardAPI/Repositories/ILeaderboardService.cs
@@ -0,0 +1,10 @@
+﻿using LeaderboardAPI.Models;
+using System.Collections.Generic;
+
+namespace LeaderboardAPI.Repositories
+{
+    public interface ILeaderboardService
+    {
+        List<Entry> getEntriesPaginated(int start, int count);
+    }
+}
---
+++ LeaderboardAPI/Repositories/LeaderboardService.cs
@@ -0,0 +1,34 @@
+﻿using LeaderboardAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaderboardAPI.Repositories
+{
+    public class LeaderboardService : ILeaderboardService
+    {
+        private readonly List<Entry> entries = new();
+
+        public LeaderboardService()
+        {
+            this.entries = new List<Entry>();
+
+        }
+
+        public List<Entry> getEntriesPaginated(int start, int count)
+        {
+            if ((start + count) > this.entries.Count() || start < 0 || start > this.entries.Count())
+            {
+                return new List<Entry>();
+            }
+
+            if (this.entries.Count() < count)
+            {
+                return this.entries;
+            }
+
+            return this.entries.GetRange(start, count);
+        }
+    }
+}

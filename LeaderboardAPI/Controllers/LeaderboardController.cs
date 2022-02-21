3c3aeed84783dbf36de495fc7913902df67d0a8b
Phillip Pham <ppthreefive@gmail.com>
2022-02-21 00:41:39 -0700

    Added LeaderboardController

---
+++ LeaderboardAPI/Controllers/LeaderboardController.cs
@@ -0,0 +1,24 @@
+﻿using LeaderboardAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaderboardAPI.Controllers
+{
+    [Route("api/leaderboard")]
+    [ApiController]
+    public class LeaderboardController : ControllerBase
+    {
+        private readonly ILeaderboardService leaderboardService;
+
+        public LeaderboardController(ILeaderboardService leaderboardService) 
+        {
+            this.leaderboardService = leaderboardService;
+        }
+
+
+    }
+}

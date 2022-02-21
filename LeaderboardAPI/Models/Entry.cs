9a9acf3f2efc30d3efd835c8bd09a3a7e26d3853
Phillip Pham <ppthreefive@gmail.com>
2022-02-20 22:54:56 -0700

    Add project files.

---
+++ LeaderboardAPI/Models/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaderboardAPI.Models
+{
+    public class Entry
+    {
+        public string username { get; private set; }
+        public int score { get; private set; }
+        public int index { get; private set; }
+
+        public Entry(string username, int score, int index)
+        {
+            this.username = username;
+            this.score = score;
+            this.index = index;
+        }
+    }
+}
---
+++ LeaderboardAPI/Models/Leaderboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaderboardAPI.Models
+{
+    public class Leaderboard
+    {
+        public List<Entry> entries { get; private set; }
+
+        public Leaderboard()
+        {
+            this.entries = new List<Entry>();
+
+        }
+
+        public List<Entry> getEntriesPaginated(int start, int count)
+        {
+            if ((start + count) > this.entries.Count() || start < 0 || start > this.entries.Count())
+            {
+                return new List<Entry>();
+            }
+
+            if (this.entries.Count() < count)
+            {
+                return this.entries;
+            }
+
+            return this.entries.GetRange(start, count);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            this.entries.ForEach(entry => {
+                result += "User: " + entry.username + ", Score: " + entry.score + "\n";
+            });
+
+            return result;
+        }
+    }
+}

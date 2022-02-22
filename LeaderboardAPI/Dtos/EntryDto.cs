61176421116a82c7d830d42805aa01d30d1c06d1
Phillip Pham <ppthreefive@gmail.com>
2022-02-21 22:19:32 -0700

    Added EntryDto to establish a proper data contract, changed the List<Entry> to IEnumerable<EntryDto>

---
+++ LeaderboardAPI/Dtos/EntryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaderboardAPI.Dtos
+{
+    public record EntryDto
+    {
+        public string username { get; init; }
+        public int score { get; init; }
+        public int index { get; init; }
+    }
+}
--- LeaderboardAPI/Dtos/LeaderboardDto.cs
+++ LeaderboardAPI/Dtos/LeaderboardDto.cs
@@ -8,17 +8,19 @@ namespace LeaderboardAPI.Dtos
 {
     public class LeaderboardDto
     {
-        public List<Entry> entries { get; set; }
+        public IEnumerable<EntryDto> entries { get; set; }
         public int page { get; set; }
         public int totalPages { get; set; }
         public int pageSize { get; set; }
+        public int subsetCount { get; set; }
 
-        public LeaderboardDto(List<Entry> entries, int page, int pageSize, int totalPages)
+        public LeaderboardDto(IEnumerable<EntryDto> entries, int page, int pageSize, int totalPages)
         {
             this.entries = entries;
             this.page = page;
             this.pageSize = pageSize;
             this.totalPages = totalPages;
+            this.subsetCount = entries.Count();
         }
     }
 }

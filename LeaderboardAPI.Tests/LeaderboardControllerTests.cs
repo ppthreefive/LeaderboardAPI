5ab927ee76ef120bcfedee736285e8f462133c6a
Phillip Pham <ppthreefive@gmail.com>
2022-02-23 17:38:24 -0700

    Added coverage for branch where results are not found

--- LeaderboardAPI.Tests/LeaderboardControllerTests.cs
+++ LeaderboardAPI.Tests/LeaderboardControllerTests.cs
@@ -11,7 +11,7 @@ namespace LeaderboardAPI.Tests
     public class LeaderboardControllerTests
     {
         [Fact]
-        public async Task GetLeaderboard_Returns_Correct_Number_Of_Entries()
+        public async Task GetLeaderboard_Returns_Correct_Page_Size()
         {
             var fakeLeaderboard = A.Fake<LeaderboardDto>();
             int count = fakeLeaderboard.pageSize;
@@ -22,6 +22,18 @@ namespace LeaderboardAPI.Tests
 
             var actionResult = await controller.GetLeaderboardAsync(null, null);
             Assert.Equal(count, actionResult.Value.pageSize);
+        }
+
+        [Fact]
+        public async Task GetLeaderboard_Returns_NotFound()
+        {
+            var service = A.Fake<ILeaderboardService>();
+
+            A.CallTo(() => service.GetEntriesHelperAsync(-1, -1, 50)).Returns(Task.FromResult<ActionResult<LeaderboardDto>>(null));
+            var controller = new LeaderboardController(service, "50");
+
+            var actionResult = await controller.GetLeaderboardAsync(-1, -1);
+            Assert.IsType<NotFoundResult>(actionResult.Result);
         }
     }
 }
